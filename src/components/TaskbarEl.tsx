3a2efc8d2985c73cb306481ecbce09d7e966b9fe
Kevin <pintokevin2002@hotmail.com>
2025-03-28 17:22:01 +0100

    Receiving one props with everything in it

--- src/components/TaskbarEl.tsx
+++ src/components/TaskbarEl.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import Image from "next/image";
+import {TaskbarItem} from "@/utils/interfaces";
 
 interface Props {
-    src: string;
-    title: string;
-    onClick: Function|null;
+    item: TaskbarItem;
 }
 
-const TaskbarEl: React.FC<Props> = ({src, title, onClick}) => {
+const TaskbarEl: React.FC<Props> = ({item}) => {
     const handleClick = (e: any): void => {
         e.preventDefault();
-        if(typeof onClick === "function") onClick(e);
+        if(typeof item.onClick === "function") item.onClick(e);
     }
 
     return (
-        <div className="taskbar-icon h-[29px] w-[29px] relative isolate" title={title} onClick={handleClick}>
+        <div className="taskbar-icon h-[29px] w-[29px] relative isolate" title={item.title} onClick={handleClick}>
             <Image
-                src={src}
-                alt={title}
+                src={item.src}
+                alt={item.title}
                 width={29}
                 height={29}
                 className="object-contain object-center"
