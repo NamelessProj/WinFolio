9c9d561cde9e2a0b97fc282dd470e91ad952e296
Kevin <pintokevin2002@hotmail.com>
2025-03-29 14:05:47 +0100

    Making sure the crown is display only when needed

--- src/components/WindowsMenuFooter.tsx
+++ src/components/WindowsMenuFooter.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 
 interface Props {
     name?: string|null;
+    displayCrown?: boolean;
 }
 
 const BUTTON_CLASS: string = "h-9 rounded-md px-1.5 py-[6px] flex justify-center items-center hover:bg-[#363636]";
 
-const WindowsMenuFooter: React.FC<Props> = ({name=null}) => {
+const WindowsMenuFooter: React.FC<Props> = ({name=null, displayCrown=false}) => {
     return (
         <div id="windows-menu-footer" className="h-[60px] w-[calc(2*var(--windows-menu-padding)+100%)] transform -translate-x-[var(--windows-menu-padding)] bg-[#222] px-[calc(var(--windows-menu-padding)*2)] mt-11">
             <div className="h-full flex justify-between items-center">
@@ -15,9 +16,11 @@ const WindowsMenuFooter: React.FC<Props> = ({name=null}) => {
                     type="button"
                     className={`${BUTTON_CLASS} relative isolate`}
                 >
-                    <svg className="text-[#DAA520FF] absolute z-20 top-[-40%] left-[20%] rotate-[20deg] pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="currentColor" height="25">
-                        <path d="M309 106c11.4-7 19-19.7 19-34c0-22.1-17.9-40-40-40s-40 17.9-40 40c0 14.4 7.6 27 19 34L209.7 220.6c-9.1 18.2-32.7 23.4-48.6 10.7L72 160c5-6.7 8-15 8-24c0-22.1-17.9-40-40-40S0 113.9 0 136s17.9 40 40 40c.2 0 .5 0 .7 0L86.4 427.4c5.5 30.4 32 52.6 63 52.6H426.6c30.9 0 57.4-22.1 63-52.6L535.3 176c.2 0 .5 0 .7 0c22.1 0 40-17.9 40-40s-17.9-40-40-40s-40 17.9-40 40c0 9 3 17.3 8 24l-89.1 71.3c-15.9 12.7-39.5 7.5-48.6-10.7L309 106z"/>
-                    </svg>
+                    {displayCrown && (
+                        <svg className="text-[#DAA520FF] absolute z-20 top-[-40%] left-[20%] rotate-[20deg] pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="currentColor" height="25">
+                            <path d="M309 106c11.4-7 19-19.7 19-34c0-22.1-17.9-40-40-40s-40 17.9-40 40c0 14.4 7.6 27 19 34L209.7 220.6c-9.1 18.2-32.7 23.4-48.6 10.7L72 160c5-6.7 8-15 8-24c0-22.1-17.9-40-40-40S0 113.9 0 136s17.9 40 40 40c.2 0 .5 0 .7 0L86.4 427.4c5.5 30.4 32 52.6 63 52.6H426.6c30.9 0 57.4-22.1 63-52.6L535.3 176c.2 0 .5 0 .7 0c22.1 0 40-17.9 40-40s-17.9-40-40-40s-40 17.9-40 40c0 9 3 17.3 8 24l-89.1 71.3c-15.9 12.7-39.5 7.5-48.6-10.7L309 106z"/>
+                        </svg>
+                    )}
                     <Image
                         src={typeof name !== "string" ? "/windows11_background.jpg" : `https://api.dicebear.com/8.x/fun-emoji/jpg?seed=${name}`}
                         alt="Profile Picture"
