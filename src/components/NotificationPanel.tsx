fe5e8f9cbdf68706a5771788df3792fcae1f6f05
Kevin <pintokevin2002@hotmail.com>
2025-03-29 11:49:02 +0100

    Passing a ref to the ClickOutside component

--- src/components/NotificationPanel.tsx
+++ src/components/NotificationPanel.tsx
@@ -11,9 +11,11 @@ interface Props {
 }
 
 const NotificationPanel: React.FC<Props> = ({isOpen, setIsOpen, notifications, handleClearNotifications}) => {
+    const ref = React.useRef<HTMLDivElement>(null);
+
     return (
-        <ClickOutside isOpen={isOpen} setIsOpen={setIsOpen}>
-            <div id="notification-panel" className={`absolute bottom-7 right-2.5 w-[325px] bg-[rgba(25,25,25,0.7)] backdrop-blur-xl text-white rounded-md transform translate-x-[350px] ${isOpen ? '!translate-x-0' : ''} transition-transform duration-200 ease-in-out`}>
+        <ClickOutside ref={ref} isOpen={isOpen} setIsOpen={setIsOpen}>
+            <div ref={ref} id="notification-panel" className={`absolute bottom-7 right-2.5 w-[325px] bg-[rgba(25,25,25,0.7)] backdrop-blur-xl text-white rounded-md transform translate-x-[350px] ${isOpen ? '!translate-x-0' : ''} transition-transform duration-200 ease-in-out`}>
                 <div className="flex justify-between items-center bg-[rgba(15,15,15,0.4)] py-2 px-4">
                     <h4>Notifications</h4>
                     <button
