c61952402a37c906cfa2151fcedd3d530775a4ea
Kevin <pintokevin2002@hotmail.com>
2025-04-03 15:28:56 +0200

    Adding the component to display what i'm not able to do (nothing)

---
+++ src/components/WhatICantDo/WhatICantDo.tsx
@@ -0,0 +1,14 @@
+const WhatICantDo = () => {
+    return (
+        <div className="text-center py-8 px-4 max-w-[440px] mx-auto">
+            <h2 className="text-2xl mb-3">
+                Error 404!
+            </h2>
+            <p className="text-balance text-gray-400">
+                Sorry, but I can do everything, did you really think I couldn't do anything?
+            </p>
+        </div>
+    );
+};
+
+export default WhatICantDo;
