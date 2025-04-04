0f0b45bb213710dec9c43c2c18294b0e0a2f984a
Kevin <pintokevin2002@hotmail.com>
2025-04-04 20:03:03 +0200

    Adding the file explorer

---
+++ src/components/FileExplorer/FileExplorer.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import {FileExplorerLocation, Program} from "@/utils/interfaces";
+import FileExplorerHeader from "@/components/FileExplorer/FileExplorerHeader";
+import FileExplorerSubHeader from "@/components/FileExplorer/FileExplorerSubHeader";
+import FileExplorerMain from "@/components/FileExplorer/FileExplorerMain";
+
+interface Props {
+    programs: Array<Program>;
+}
+
+const FileExplorer: React.FC<Props> = ({programs}) => {
+    const [location, setLocation] = React.useState<string>("Desktop");
+    const [locationsList, setLocationsList] = React.useState<Array<FileExplorerLocation[]>>([
+        [
+            {
+                name: "Home",
+                src: "/Computer.png",
+                alt: "Home",
+                isActive: false,
+                content: [],
+            },
+            {
+                name: "No Drive",
+                src: "/Desktop.png",
+                alt: "OneDrive",
+                isActive: false,
+                content: [],
+            },
+        ],
+        [
+            {
+                name: "Desktop",
+                src: "/Desktop.png",
+                alt: "Desktop",
+                isActive: true,
+                content: [],
+            },
+            {
+                name: "Downloads",
+                src: "/Computer.png",
+                alt: "Downloads",
+                isActive: false,
+                content: [],
+            },
+            {
+                name: "Documents",
+                src: "/Computer.png",
+                alt: "Documents",
+                isActive: false,
+                content: [],
+            },
+            {
+                name: "Images",
+                src: "/Computer.png",
+                alt: "Images",
+                isActive: false,
+                content: [],
+            },
+            {
+                name: "Music",
+                src: "/Computer.png",
+                alt: "Music",
+                isActive: false,
+                content: [],
+            },
+            {
+                name: "Videos",
+                src: "/Computer.png",
+                alt: "Videos",
+                isActive: false,
+                content: [],
+            },
+        ]
+    ]);
+
+    const handleLocationChange = (newLocation: string): void => {
+        setLocation(newLocation);
+        const updatedLocationsList = locationsList.map((location: FileExplorerLocation[]) => {
+            return location.map((item: FileExplorerLocation) => {
+                return {
+                    ...item,
+                    isActive: item.name === newLocation,
+                };
+            });
+        });
+        setLocationsList(updatedLocationsList);
+    }
+
+    return (
+        <div>
+            <FileExplorerHeader location={location} />
+            <FileExplorerSubHeader />
+            <FileExplorerMain
+                locationsList={locationsList}
+                handleLocationChange={handleLocationChange}
+            />
+        </div>
+    );
+};
+
+export default FileExplorer;
